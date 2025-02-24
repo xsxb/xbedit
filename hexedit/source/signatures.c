96805071bfd19d46f332c0888d377424131a96f0
xsxb <ruoyu100@163.com>
2025-02-24 06:29:46 -0600

    Rework get_signature(), sigcmp()

--- hexedit/source/signatures.c
+++ hexedit/source/signatures.c
@@ -1,37 +1,43 @@
 #include "signatures.h"
 
-void get_signature(FILE *fptr)
+// arg1 = buffer from file read; arg2 = pointer to signature; arg3 = length of signature
+// TODO: Error handling
+int sigcmp(unsigned char *sig_buffer, const int signature[], int sig_size)
 {
-	char *format;
-	format = "Unrecognized binary";
-	unsigned char s_buffer[8];
-	int recognized = 0;
+	int is_equal = 0;
 
-	fgets(s_buffer, 8, fptr);
-	//rewind(fptr);
+	//if (sizeof signature > sig_size) {}
 
-	// Is DOS
-	if (!recognized)
+	for (int i = 0; i < sig_size; i++)
 	{
-		for (int c = 0; c < 2; c++)
+		//printf("Comparing: %02X | %02X\n", sig_buffer[i], signature[i]);
+		if (sig_buffer[i] == signature[i])
 		{
-			if (s_buffer[c] == SIG_DOS[c])
+			if (i == sig_size -1)
 			{
-				if (c == 1) { format = "DOS"; recognized = 1;}
+				is_equal = 1;
 			}
 		}
 	}
 
-	// Is ELF
-	if (!recognized)
+	return is_equal;
+}
+
+void get_signature(FILE *fptr)
+{
+	const char *format;
+	unsigned char s_buffer[8];
+
+	format = "Unrecognized binary";
+
+	fgets(s_buffer, 8, fptr);
+	
+	//printf("Size of pointer array: %d\n", sizeof SIGNATURE_P_ARRAY);
+	for (int s = 0; s < (sizeof SIG_P_ARR / 8); s++)
 	{
-		for (int c = 0; c < 4; c++)
-		{
-			if (s_buffer[c] == SIG_ELF[c])
-			{
-				if (c == 3) { format = "ELF"; recognized = 1; }
-			}
-		}
+		int n = sigcmp(s_buffer, SIG_P_ARR[s], (sizeof SIG_P_ARR[s] / 4));
+		
+		if (n == 1) { format = SIG_P_STR[s]; }
 	}
 
 	printf("Format: %s\n", format);
--- hexedit/source/signatures.h
+++ hexedit/source/signatures.h
@@ -2,10 +2,11 @@
 #define SIGNATURES_H
 
 // Binary signature definitions
-//
-// Arrays with #define directive?
 
 const int SIG_DOS[2] = {0x4D, 0x5A};
 const int SIG_ELF[4] = {0x7F, 0x45, 0x4C, 0x46};
+
+const int *SIG_P_ARR[2] = { SIG_DOS, SIG_ELF };
+const char SIG_P_STR[2][8] = { "DOS", "ELF" };
 
 #endif
